ab8d1976999c18bae83ac2e2d17e7689aa2a4b7a
Nitish Barya <barya1993@gmail.com>
2022-11-13 19:35:07 -0500

    Create main.tf

---
+++ temprature-challange-terraform/main.tf
@@ -0,0 +1,36 @@
+##test instance
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.41.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "secure-proxy-367022"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
