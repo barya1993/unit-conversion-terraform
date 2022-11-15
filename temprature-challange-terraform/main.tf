f669f26bc81b2c3497e177f1655d6c0f2c33a777
Nitish Barya <barya1993@gmail.com>
2022-11-14 22:21:16 -0500

    making few changes in the bucket code

--- temprature-challange-terraform/main.tf
+++ temprature-challange-terraform/main.tf
@@ -1,4 +1,3 @@
-##gcs bucket for saving the state files of terraform
 terraform {
   required_providers {
     google = {
@@ -9,13 +8,29 @@ terraform {
 }
 
 provider "google" {
+  credentials = file("/Users/nbarya/Downloads/secure-proxy-367022-c486c4b23209.json")
+
   project = "secure-proxy-367022"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
 
 module "bucket" {
-  name       = "terraform-bucket"
+  source     = "terraform-google-modules/cloud-storage/google"
+  project_id = "secure-proxy-367022"
+  location   = "us-central1"
+  names  = ["bucket"]
+  prefix =  "terraform"
+  set_admin_roles = true
+  bucket_admins = {
+    second = "serviceAccount:cicdterraformsvc@secure-proxy-367022.iam.gserviceaccount.com"
+  }
+  labels = {
+    tribe         = "platform"
+    env           = "production"
+    public        = "no"
+    application_name = "terraform_state_bucket"
+  }
   lifecycle_rules = [{
     action = {
       type = "Delete"
@@ -25,9 +40,3 @@ module "bucket" {
       with_state = "ANY"
     }
   }]
-
-  iam_members = [{
-    role   = "roles/storage.admin"
-    member = "serviceAccount:$cicdterraformsvc@secure-proxy-367022.iam.gserviceaccount.com"
-  }]
-}
