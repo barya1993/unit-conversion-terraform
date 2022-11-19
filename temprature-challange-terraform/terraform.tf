5cd7b62c82f3919e27749f98969d3a5f21175370
Nitish Barya <barya1993@gmail.com>
2022-11-18 22:23:26 -0500

    Update terraform.tf

--- temprature-challange-terraform/terraform.tf
+++ temprature-challange-terraform/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket  = "terraform-us-central1-bucket"
-    prefix  = "terraform/state"
+    bucket = "terraform-us-central1-bucket"
+    prefix = "terraform/state"
   }
 }
